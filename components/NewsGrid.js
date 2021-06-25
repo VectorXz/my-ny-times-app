c9290e32171510f930ad85e4d2ac1413acdcef67
vectorxz <vectorx2263@hotmail.com>
2021-06-25 13:26:04 +0700

    update grid

--- components/NewsGrid.js
+++ components/NewsGrid.js
@@ -10,7 +10,7 @@ export default function NewsGrid(props) {
     })
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-12 gap-y-10 my-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-10 my-3">
             {data_filtered.length > 0 && data_filtered.map((elem, i) => {
                 const imageUrl = elem.media.length > 0 ? elem.media[0]['media-metadata'][2].url : "/defaultImage.png"
                 const imageAlt = elem.media.length > 0 ? `${elem.media[0]['caption']} (c) ${elem.media[0]['copyright']}` : "/defaultImage.png"
